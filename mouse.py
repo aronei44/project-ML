3801d78de7612582b309b9b664e92d8c2c60c833
aronei44 <meliodasmeliodas224@gmail.com>
2021-11-28 16:58:48 +0700

    Speech recognizer

---
+++ mouse.py
@@ -0,0 +1,51 @@
+from handDetection import HandDetection
+import cv2
+import math
+from pymouse import PyMouse
+import numpy as np
+import keyboard
+
+monitor_height = 768
+monitor_width = 1366
+
+handDetection = HandDetection(min_detection_confidence=0.5,min_tracking_confidence=0.5)
+
+
+webcam = cv2.VideoCapture()
+webcam.open(0,cv2.CAP_DSHOW)
+
+m = PyMouse()
+
+while True:
+    status, frame = webcam.read()
+    frame = cv2.flip(frame,1)
+    handLandMarks = handDetection.findHandLandMarks(image=frame, draw=True)
+
+    if len(handLandMarks) != 0:
+        x4, y4 = handLandMarks[4][1],handLandMarks[4][2]
+        x5, y5 = handLandMarks[5][1],handLandMarks[5][2]
+        x8, y8 = handLandMarks[8][1],handLandMarks[8][2]
+        x12, y12 = handLandMarks[12][1],handLandMarks[12][2]
+
+        pos_x = x8*monitor_width/frame.shape[1]
+        pos_y = y8*monitor_height/frame.shape[0]
+
+        thumb = math.hypot(x4-x5,y4-y5)
+        left = math.hypot(x8-x12,y8-y12)
+        # print(thumb)
+        if thumb >50:
+            m.click(int(pos_x), int(pos_y),1)
+            thumb = 49
+
+        if left < 50:
+            keyboard.press_and_release('left')
+        m.move(int(pos_x), int(pos_y))
+        
+
+
+    cv2.imshow("Hand Landmark", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+webcam.release()
---
+++ speech.py
@@ -0,0 +1,22 @@
+import speech_recognition
+import pyttsx3
+
+recognizer = speech_recognition.Recognizer()
+
+while True:
+
+    try:
+        with speech_recognition.Microphone() as mic:
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            audio = recognizer.listen(mic)
+
+            text = recognizer.recognize_google(audio_data=audio,language="en-US")
+            text = text.lower()
+
+            print(f"Recognized: {text}")
+            if text == 'stop':
+                break
+
+    except :
+        recognizer = speech_recognition.Recognizer()
+        continue
