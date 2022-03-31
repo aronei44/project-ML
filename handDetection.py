4711ef28e6d2731db3d37d249c4ed0293b77e00c
aronei44 <meliodasmeliodas224@gmail.com>
2022-03-31 19:47:16 +0700

    refactory (HandDetection) : set max num hands to 1

--- handDetection.py
+++ handDetection.py
@@ -4,40 +4,42 @@ import cv2
 mpHands = mp.solutions.hands
 mpDraw = mp.solutions.drawing_utils
 
+
 class HandDetection:
-    def __init__(self, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5):
-        self.hands = mpHands.Hands(max_num_hands=max_num_hands,min_detection_confidence=min_detection_confidence,min_tracking_confidence=min_tracking_confidence)
+    def __init__(self, max_num_hands=1, min_detection_confidence=0.5, min_tracking_confidence=0.5):
+        self.hands = mpHands.Hands(max_num_hands=max_num_hands, min_detection_confidence=min_detection_confidence,
+                                   min_tracking_confidence=min_tracking_confidence)
 
-
-    def findHandLandMarks(self,image,handNumber=0,draw=False):
+    def findHandLandMarks(self, image, handNumber=0, draw=False):
         originalImage = image
-        image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = self.hands.process(image)
 
         landMarkList = []
 
         if results.multi_hand_landmarks:
             for hand in results.multi_hand_landmarks:
-                for id,landMark in enumerate(hand.landmark):
+                for id, landMark in enumerate(hand.landmark):
                     imgH, imgW, imgC = originalImage.shape
-                    xPos, yPos = int(landMark.x*imgW),int(landMark.y*imgH)
-                    landMarkList.append([id,xPos,yPos])
+                    xPos, yPos = int(landMark.x*imgW), int(landMark.y*imgH)
+                    landMarkList.append([id, xPos, yPos])
                 if draw:
-                    mpDraw.draw_landmarks(originalImage,hand, mpHands.HAND_CONNECTIONS)
+                    mpDraw.draw_landmarks(
+                        originalImage, hand, mpHands.HAND_CONNECTIONS)
 
         return landMarkList
 
 
-if __name__=='__main__':
-    handDetection = HandDetection(min_detection_confidence=0.5,min_tracking_confidence=0.5)
-
+if __name__ == '__main__':
+    handDetection = HandDetection(
+        min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
     webcam = cv2.VideoCapture()
-    webcam.open(0,cv2.CAP_DSHOW)
+    webcam.open(0, cv2.CAP_DSHOW)
 
     while True:
         status, frame = webcam.read()
-        frame = cv2.flip(frame,1)
+        frame = cv2.flip(frame, 1)
         handLandMarks = handDetection.findHandLandMarks(image=frame, draw=True)
         print(handLandMarks)
 
